baac01c997aad04768e5aec3b579420cbbf92be6
Constantin <cofritzsch@gmail.com>
2022-10-18 23:30:48 +0200

    Version 2.2.1 - filled InputChecks with Methods from Program.

--- src/main/java/de/st/app/Program.java
+++ src/main/java/de/st/app/Program.java
@@ -1,5 +1,6 @@
 package de.st.app;
 
+import de.st.io.InputChecks;
 import de.st.io.NumberOutput;
 import de.st.logic.Primes;
 
@@ -8,88 +9,40 @@ import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.List;
 
 /**
  *
  */
 public class Program {
-    private static int numberOfPrimes;
-    private static String filename;
 
     /**
      * @param args uebergibt die Anzahl der Primzahlen und einen (optionalen) Dateinamen
      */
     public static void main(String[] args) {
         Primes p = new Primes();
-        if (checkArguments(args)) {
-            Path path = Path.of("src/textfiles/" + filename + ".txt");
+        if (InputChecks.checkArguments(args)) {
+            Path path = Path.of("src/textfiles/" + InputChecks.getFilename() + ".txt");
             try {
-                new NumberOutput(Files.newBufferedWriter(path, StandardOpenOption.CREATE_NEW)).writeInFile(p.generatePrimes(numberOfPrimes));
+                List<Integer> primeList = p.generatePrimes(InputChecks.getNumberOfPrimes());
+                new NumberOutput(Files.newBufferedWriter(path, StandardOpenOption.CREATE_NEW)).writeInFile(primeList);
             } catch (FileAlreadyExistsException f) {
                 System.err.println("File already exist");
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-            System.out.printf("NOP: %S, Name: %s%n", numberOfPrimes, filename);
+            System.out.printf("NOP: %S, Name: %s%n", InputChecks.getNumberOfPrimes(), InputChecks.getFilename());
         } else {
             System.err.println("Wrong arguments were committed");
         }
     }
 
     /**
-     * Checkt die Argumente (String-Array) auf Primzahlen (in Feld 1) und einen Dateinamen (in Feld 2)
-     * Falls kein Dateiname gesetzt wurde, wird ein default name benutzt.
-     * Per Short-Circuit Technik werden die jeweiligen Faktoren geprüft.
-     *
-     * @param args uebergibt die Anzahl der Primzahlen und einen (optionalen) Dateinamen
-     * @return true, wenn die Argumente die Bedingungen erfüllen
-     */
-    public static boolean checkArguments(String[] args) {
-        return (!isArgsOneEmpty(args) && isArgsOneNumber(args)) && (!isArgsTwoEmpty(args) || setDefaultName());
-    }
-
-    /**
-     * Ueberprueft, ob das Feld 0 leer ist.
-     *
-     * @param args uebergibt die Anzahl der Primzahlen und einen (optionalen) Dateinamen
-     * @return True, wenn args eine länge von 0 hat. (= leeres Array)
-     */
-    private static boolean isArgsOneEmpty(String[] args) {
-        return args.length == 0;
-    }
-
-    /**
-     * @param args uebergibt die Anzahl der Primzahlen und einen (optionalen) Dateinamen
-     * @return true, wenn in den Args auf Feld 0 eine Zahl drin ist.
-     */
-    private static boolean isArgsOneNumber(String[] args) {
-        try {
-            numberOfPrimes = Integer.parseInt(args[0]);
-        } catch (NumberFormatException e) {
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * Wenn ein Dateiname gesetzt wurde, wird dieser der Variable übergeben.
-     *
-     * @param args uebergibt die Anzahl der Primzahlen und einen (optionalen) Dateinamen
-     * @return true, wenn das 2. Argument leer ist.
-     */
-    private static boolean isArgsTwoEmpty(String[] args) {
-        if (args.length == 1) return true;
-        filename = args[1];
-        return false;
-    }
-
-    /**
      * setzt den Standard Dateinamen, wenn kein anderer gesetzt wurde.
-     *
      * @return true
      */
-    private static boolean setDefaultName() {
-        filename = "new_document";
+    public static boolean setDefaultName() {
+        InputChecks.setFilename("new_document");
         return true;
     }
 }
--- src/main/java/de/st/io/InputChecks.java
+++ src/main/java/de/st/io/InputChecks.java
@@ -1,14 +1,65 @@
 package de.st.io;
 
-public class InputChecks {
+import de.st.app.Program;
 
-    public static boolean checkNumber(String arg){
-        try{
-            Integer.parseInt(arg);
-        }catch(NumberFormatException nfe){
-            System.exit(1);
+public class InputChecks {
+    static int numberOfPrimes;
+    static String filename;
+
+    /**
+     * Checkt die Argumente (String-Array) auf Primzahlen (in Feld 1) und einen Dateinamen (in Feld 2)
+     * Falls kein Dateiname gesetzt wurde, wird ein default name benutzt.
+     * Per Short-Circuit Technik werden die jeweiligen Faktoren geprüft.
+     * @param args uebergibt die Anzahl der Primzahlen und einen (optionalen) Dateinamen
+     * @return true, wenn die Argumente die Bedingungen erfüllen
+     */
+    public static boolean checkArguments(String[] args) {
+        return (!isArgsOneEmpty(args) && isArgsOneNumber(args)) && (!isArgsTwoEmpty(args) || Program.setDefaultName());
+    }
+
+
+    /**
+     * Ueberprueft, ob das Feld 0 leer ist.
+     * @param args uebergibt die Anzahl der Primzahlen und einen (optionalen) Dateinamen
+     * @return True, wenn args eine länge von 0 hat. (= leeres Array)
+     */
+    private static boolean isArgsOneEmpty(String[] args) {
+        return args.length == 0;
+    }
+
+    /**
+     * @param args uebergibt die Anzahl der Primzahlen und einen (optionalen) Dateinamen
+     * @return true, wenn in den Args auf Feld 0 eine Zahl drin ist.
+     */
+    private static boolean isArgsOneNumber(String[] args) {
+        try {
+            numberOfPrimes = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            return false;
         }
+        return true;
+    }
+
+    /**
+     * Wenn ein Dateiname gesetzt wurde, wird dieser der Variable übergeben.
+     * @param args uebergibt die Anzahl der Primzahlen und einen (optionalen) Dateinamen
+     * @return true, wenn das 2. Argument leer ist.
+     */
+    private static boolean isArgsTwoEmpty(String[] args) {
+        if (args.length == 1) return true;
+        filename = args[1];
         return false;
     }
 
+    public static int getNumberOfPrimes(){
+        return numberOfPrimes;
+    }
+
+    public static String getFilename() {
+        return filename;
+    }
+
+    public static void setFilename(String filename) {
+        InputChecks.filename = filename;
+    }
 }
