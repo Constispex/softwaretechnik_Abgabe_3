c45b7e6cd62d772b83812f7b7b7f04eac473d718
Constantin <cofritzsch@gmail.com>
2022-10-18 23:03:47 +0200

    Version 2.2 - works but absolutely not fine

--- src/main/java/de/st/app/Program.java
+++ src/main/java/de/st/app/Program.java
@@ -1,11 +1,13 @@
 package de.st.app;
 
+import de.st.io.NumberOutput;
 import de.st.logic.Primes;
-import java.io.File;
-import java.io.FileWriter;
+
 import java.io.IOException;
-import java.io.Writer;
-import java.util.List;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 /**
  *
@@ -20,22 +22,17 @@ public class Program {
     public static void main(String[] args) {
         Primes p = new Primes();
         if (checkArguments(args)) {
-            Writer writer;
-            File file = new File("src/textfiles/" + filename + ".txt");
+            Path path = Path.of("src/textfiles/" + filename + ".txt");
             try {
-                if (!file.exists()) {
-                    writer = new FileWriter(file);
-                    List<Integer> primeList = p.generatePrimes(numberOfPrimes);
-                    // TODO: Writer und primeList übergeben
-                } else System.err.println("File already exist!");
+                new NumberOutput(Files.newBufferedWriter(path, StandardOpenOption.CREATE_NEW)).writeInFile(p.generatePrimes(numberOfPrimes));
+            } catch (FileAlreadyExistsException f) {
+                System.err.println("File already exist");
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-            // new NumberOutput(writer, p.generatePrimes(numberOfPrimes));
-
             System.out.printf("NOP: %S, Name: %s%n", numberOfPrimes, filename);
         } else {
-            System.out.println("Wrong arguments were committed");
+            System.err.println("Wrong arguments were committed");
         }
     }
 
@@ -43,8 +40,9 @@ public class Program {
      * Checkt die Argumente (String-Array) auf Primzahlen (in Feld 1) und einen Dateinamen (in Feld 2)
      * Falls kein Dateiname gesetzt wurde, wird ein default name benutzt.
      * Per Short-Circuit Technik werden die jeweiligen Faktoren geprüft.
+     *
      * @param args uebergibt die Anzahl der Primzahlen und einen (optionalen) Dateinamen
-     * @return
+     * @return true, wenn die Argumente die Bedingungen erfüllen
      */
     public static boolean checkArguments(String[] args) {
         return (!isArgsOneEmpty(args) && isArgsOneNumber(args)) && (!isArgsTwoEmpty(args) || setDefaultName());
@@ -52,6 +50,7 @@ public class Program {
 
     /**
      * Ueberprueft, ob das Feld 0 leer ist.
+     *
      * @param args uebergibt die Anzahl der Primzahlen und einen (optionalen) Dateinamen
      * @return True, wenn args eine länge von 0 hat. (= leeres Array)
      */
@@ -74,6 +73,7 @@ public class Program {
 
     /**
      * Wenn ein Dateiname gesetzt wurde, wird dieser der Variable übergeben.
+     *
      * @param args uebergibt die Anzahl der Primzahlen und einen (optionalen) Dateinamen
      * @return true, wenn das 2. Argument leer ist.
      */
@@ -85,6 +85,7 @@ public class Program {
 
     /**
      * setzt den Standard Dateinamen, wenn kein anderer gesetzt wurde.
+     *
      * @return true
      */
     private static boolean setDefaultName() {
--- src/main/java/de/st/io/NumberOutput.java
+++ src/main/java/de/st/io/NumberOutput.java
@@ -1,16 +1,19 @@
 package de.st.io;
 
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.Writer;
+import java.util.List;
 
 public class NumberOutput extends BufferedWriter {
-    private String _filename;
-
-    public NumberOutput(Writer writer, String filename) {
+    public NumberOutput(Writer writer) {
         super(writer);
-        _filename = filename;
     }
 
-
-
+    public void writeInFile(List<Integer> list) throws IOException {
+        for (Integer i : list) {
+            super.write((i) + " ");
+        }
+        super.close();
+    }
 }
